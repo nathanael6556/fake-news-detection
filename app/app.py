a25d84ea13b80e3c118153b8f08adc1f6ca3dcfd
Nathanael <nathanael6556@gmail.com>
2024-10-22 00:50:11 +0700

    feat: web app support url mode

--- app/app.py
+++ app/app.py
@@ -1,7 +1,37 @@
+import requests
+from bs4 import BeautifulSoup
 from flask import Flask, render_template, request
 import model_evaluation
 import data_preprocessing
 
+
+def get_prediction(text, mode):
+    result = None
+    y_percent = 0  # Inisialisasi y_percent dengan nilai default
+    y_status = None
+
+    if not text:
+        return result, y_percent, y_status
+    
+    if mode == "url":
+        url = text
+        response = requests.get(url)
+        soup = BeautifulSoup(response.content, features="lxml")
+        text = soup.get_text(separator="\n")
+        
+    text = data_preprocessing.clean_text(text)
+        
+    y_pred, y_status = model_evaluation.vote(text)
+    
+    if len(y_pred) > 0:  # Pastikan bahwa y_pred tidak kosong
+        y_percent = (sum(y_pred) / 7) * 100  # Hitung persentase
+    
+    if sum(y_pred) / len(y_pred) > 0.5:
+        result = 'Fake News Detected'
+    else:
+        result = 'News Looks Real'
+    
+    return result, y_percent, y_status
 
 app = Flask(__name__)
 
@@ -12,20 +42,9 @@ def home():
     y_status = None
 
     if request.method == 'POST':
-        news_text = request.form['news']
-        
-        if news_text: 
-            news_text = data_preprocessing.clean_text(news_text)
-            
-            y_pred, y_status = model_evaluation.vote(news_text)
-            
-            if len(y_pred) > 0:  # Pastikan bahwa y_pred tidak kosong
-                y_percent = (sum(y_pred) / 7) * 100  # Hitung persentase
-            
-            if sum(y_pred) / len(y_pred) > 0.5:
-                result = 'Fake News Detected'
-            else:
-                result = 'News Looks Real'
+        text = request.form['news']
+        mode = request.form['mode']
+        result, y_percent, y_status = get_prediction(text, mode)
     
     return render_template('index.html', result=result, y_percent=y_percent, y_status=y_status) 
 
